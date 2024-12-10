b4eae0fba39abbe579f01928a6f4de689268e666
coleatch <cole.atchison@willowtreeapps.com>
2024-12-10 16:41:57 -0500

    Update feedback modal to use note context, copy button

--- web/src/components/modals/FeedbackModal.tsx
+++ web/src/components/modals/FeedbackModal.tsx
@@ -1,23 +1,31 @@
+import { useContext } from 'react';
+
 import { Close } from '@mui/icons-material';
 import { Button, Card, Modal, Typography, useMediaQuery } from '@mui/material';
-import { grey } from '@mui/material/colors';
+import { blue, grey } from '@mui/material/colors';
 
+import { NoteContext } from '@/context/note-context/NoteContext';
 import config from '@/services/config';
 
+import CopyButton from '../Buttons/CopyButton';
 import { FlexBox, FullSizeCenteredFlexBox } from '../styled';
 
 interface ConsentModalProps {
   isOpen: boolean;
   toggleModal: () => void;
-  sessionId: string;
 }
 
-export default function FeedbackModal({ isOpen, toggleModal, sessionId }: ConsentModalProps) {
-  const small = useMediaQuery('(max-width: 625px)');
+export default function FeedbackModal({ isOpen, toggleModal }: ConsentModalProps) {
+  const small = useMediaQuery('(max-width: 493px)');
+
+  const { startTime, endTime, sessionId } = useContext(NoteContext);
 
   const handleClose = () => {
     toggleModal();
   };
+
+  const start = startTime || new Date();
+  const end = endTime || new Date();
 
   return (
     <Modal
@@ -52,24 +60,54 @@ export default function FeedbackModal({ isOpen, toggleModal, sessionId }: Consen
             borderBottom={1}
             borderColor={grey[300]}
           >
-            <FlexBox
-              flexDirection={small ? 'column' : 'row'}
-              alignItems={small ? 'flex-start' : 'center'}
-              justifyContent={small ? 'flex-start' : 'center'}
-              gap={small ? '0.25rem' : '1rem'}
-            >
-              <Typography variant="h6">Feedback Form</Typography>
-              <Typography variant="body1">Session ID: {sessionId}</Typography>
-            </FlexBox>
+            <Typography variant="h6">Feedback Form</Typography>
             <Button variant="text" onClick={toggleModal} endIcon={<Close />}>
               Close
             </Button>
           </FlexBox>
-          <FlexBox flex={'flex'} flexDirection={'column'}>
+          <FlexBox
+            flexDirection={'row'}
+            width={'100%'}
+            paddingInline="1.5rem"
+            justifyContent={'flex-start'}
+            alignItems={'center'}
+            gap={small ? '0' : '1rem'}
+            borderBottom={1}
+            borderColor={grey[300]}
+            sx={{
+              backgroundColor: grey[50],
+            }}
+          >
+            <Typography variant="h6" fontSize={'0.8rem'}>
+              {small ? 'Copy:' : 'Click to copy:'}
+            </Typography>
+            <FlexBox
+              flexDirection={'row'}
+              alignItems={small ? 'flex-start' : 'center'}
+              justifyContent={small ? 'flex-start' : 'center'}
+              gap={small ? '0rem' : '1rem'}
+              fontSize={'0.25rem'}
+            >
+              <CopyButton copyValue={sessionId} buttonText="Session ID" />
+              <CopyButton copyValue={start.toString()} buttonText="Start Time" />
+              <CopyButton copyValue={end.toString()} buttonText="End Time" />
+            </FlexBox>
+          </FlexBox>
+          <FlexBox
+            flex={'flex'}
+            flexDirection={'column'}
+            sx={{
+              backgroundColor: blue[50] + '80',
+            }}
+          >
             <iframe
-              src={`https://docs.google.com/forms/d/e/${config.feedback.formId}/viewform?embedded=true&usp=pp_url&entry.503004240=${sessionId}`}
+              src={`https://docs.google.com/forms/d/e/${
+                config.feedback.formId
+              }/viewform?embedded=true&usp=pp_url&entry.667875168=${
+                sessionId || ''
+              }&entry.32563794=${start}&entry.1618706420=${end}`}
               width="640"
-              height="2178"
+              height="2579"
               style={{
                 display: 'flex',
                 flexDirection: 'column',
