4c618dfed34debbbbbefb252229df84e7592bec7
coleatch <cole.atchison@willowtreeapps.com>
2024-12-10 10:56:45 -0500

    Add feedback button

--- web/src/pages/NewNote/NewNote.tsx
+++ web/src/pages/NewNote/NewNote.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+
+import { Button, useMediaQuery } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
 import Meta from '@/components/Meta';
+import FeedbackModal from '@/components/modals/FeedbackModal';
 import DictatedNotes from '@/components/notes/DictatedNotes';
 import PatientHandout from '@/components/notes/PatientHandout';
 import SOAPNotes from '@/components/notes/SOAPNotes';
@@ -8,13 +12,42 @@ import { FlexBox } from '@/components/styled';
 import NoteContextProvider from '@/context/note-context/NoteContext';
 
 function NewNote() {
+  const small = useMediaQuery('(max-width: 500px)');
+  const [feebackModalOpen, setFeedbackModalOpen] = useState(false);
+
+  const toggleFeedbackModal = () => {
+    setFeedbackModalOpen(!feebackModalOpen);
+  };
+
   return (
     <>
+      <FeedbackModal
+        isOpen={feebackModalOpen}
+        toggleModal={toggleFeedbackModal}
+        sessionId="qwertyuiop1234567890"
+      />
       <Meta title="New Note" />
       <FlexBox flexDirection="column" justifyContent="center" width="100%" height="fit-content">
-        <Typography variant="h5" fontWeight="medium" marginBlock="1rem" textAlign={'center'}>
-          Dictate New Note
-        </Typography>
+        <FlexBox
+          position={'relative'}
+          flexDirection={small ? 'column' : 'row'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          marginBlock={'1rem'}
+          marginInline={'1rem'}
+        >
+          <Typography variant="h5" fontWeight="medium" marginBottom={0} textAlign={'center'}>
+            Dictate New Note
+          </Typography>
+
+          <Button
+            variant="text"
+            onClick={toggleFeedbackModal}
+            sx={{ position: small ? '' : 'absolute', right: small ? '' : 0 }}
+          >
+            Give Feedback
+          </Button>
+        </FlexBox>
         <FlexBox
           flexDirection="row"
           flexWrap="wrap"
