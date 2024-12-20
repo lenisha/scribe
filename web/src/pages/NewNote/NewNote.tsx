e67efdb8f6ef069487bc504d66d4f54e03b02987
coleatch <cole.atchison@willowtreeapps.com>
2024-12-20 15:11:25 -0500

    Refactor file to add the tabs

--- web/src/pages/NewNote/NewNote.tsx
+++ web/src/pages/NewNote/NewNote.tsx
@@ -1,33 +1,92 @@
+import { SyntheticEvent, useState } from 'react';
+
+import { Description, Notes } from '@mui/icons-material';
+import MicIcon from '@mui/icons-material/Mic';
+import { TabContext } from '@mui/lab';
+import { Tab, Tabs, useMediaQuery } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
 import Meta from '@/components/Meta';
-import DictatedNotes from '@/components/notes/DictatedNotes';
+import ClinicalNoteControls from '@/components/control-panels/ClinicalNoteControls';
+import PatientHandoutControls from '@/components/control-panels/PatientHandoutControls';
+import TranscriptionControls from '@/components/control-panels/TranscriptionControls';
+import ClinicalNote from '@/components/notes/ClinicalNote';
 import PatientHandout from '@/components/notes/PatientHandout';
-import SOAPNotes from '@/components/notes/SOAPNotes';
+import Transcription from '@/components/notes/Transcription';
 import { FlexBox } from '@/components/styled';
+import SessionTabPanel from '@/components/tabs/SessionTabPanel';
+import DictationContextProvider from '@/context/dictation-context/DictationContext';
 import NoteContextProvider from '@/context/note-context/NoteContext';
 
 function NewNote() {
+  const showOnlyIcons = useMediaQuery('(max-width: 445px)');
+  const [tabValue, setTabValue] = useState('0');
+
+  const handleTabChangeEvent = (event: SyntheticEvent<Element, Event>, value: string) => {
+    event.preventDefault();
+    setTabValue(() => value);
+  };
+
   return (
     <>
       <Meta title="New Note" />
       <FlexBox flexDirection="column" justifyContent="center" width="100%" height="fit-content">
         <Typography variant="h5" fontWeight="medium" marginBlock="1rem" textAlign={'center'}>
-          AI Scribe
+          AI Scribe Note
         </Typography>
-        <FlexBox
-          flexDirection="row"
-          flexWrap="wrap"
-          paddingInline="1rem"
-          gap="1rem"
-          height="100%"
-          width="100%"
-        >
-          <NoteContextProvider defaultLanguage={navigator.language}>
-            <DictatedNotes />
-            <SOAPNotes />
-            <PatientHandout />
-          </NoteContextProvider>
+        <FlexBox flexDirection="row" justifyContent="center" height="100%" width="100%">
+          <FlexBox
+            flexDirection="column"
+            flexWrap="wrap"
+            height="100%"
+            width="100%"
+            maxWidth="90rem"
+          >
+            <TabContext value={tabValue}>
+              <FlexBox justifyContent="center" width="100%">
+                <Tabs value={tabValue} onChange={handleTabChangeEvent}>
+                  <Tab
+                    value={'0'}
+                    label={showOnlyIcons ? '' : 'Transcription'}
+                    icon={<MicIcon />}
+                    aria-label="Transcription tab"
+                  />
+                  <Tab
+                    value={'1'}
+                    label={showOnlyIcons ? '' : 'Clinical Note'}
+                    icon={<Notes />}
+                    aria-label="Clinical Note tab"
+                  />
+                  <Tab
+                    value={'2'}
+                    label={showOnlyIcons ? '' : 'Patient Handout'}
+                    icon={<Description />}
+                    aria-label="Patient Handout tab"
+                  />
+                </Tabs>
+              </FlexBox>
+
+              <NoteContextProvider defaultLanguage={navigator.language}>
+                <DictationContextProvider>
+                  <SessionTabPanel
+                    tabValue={'0'}
+                    controlPanel={<TranscriptionControls />}
+                    note={<Transcription />}
+                  />
+                </DictationContextProvider>
+                <SessionTabPanel
+                  tabValue={'1'}
+                  controlPanel={<ClinicalNoteControls />}
+                  note={<ClinicalNote />}
+                />
+                <SessionTabPanel
+                  tabValue={'2'}
+                  controlPanel={<PatientHandoutControls />}
+                  note={<PatientHandout />}
+                />
+              </NoteContextProvider>
+            </TabContext>
+          </FlexBox>
         </FlexBox>
       </FlexBox>
     </>
